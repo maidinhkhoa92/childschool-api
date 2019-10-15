d56fd64760f431c612f6f649837d3a2d33e606c1
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2019-10-15 23:35:04 +0700

    Update api

--- app/services/messages.js
+++ app/services/messages.js
@@ -7,6 +7,8 @@ const create = async (from_user, to_user = [], message ) => {
     const ref = firebase.collection("messages");
     let result = [];
     await _.forEach(to_user, item => {
+        item.id = item._id;
+        delete item._id;
         const params = {
             from_user: from_user,
             to_user: item,

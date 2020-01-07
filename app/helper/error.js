7f10e67b357ccb5889dc86375dbe556df1ec2952
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2020-01-07 23:03:36 +0700

    update message

--- app/helper/error.js
+++ app/helper/error.js
@@ -1,7 +1,12 @@
 const error_data = require('./error.json');
 module.exports = function (err, req, res, next) {
   if (err.name === "ValidationError") {
-    res.boom.badRequest(err.errors[0].messages[0]);
+    console.log(err)
+    if( err.errors[0].messages[0] === '"secondTeacher" contains an invalid value'){
+      res.boom.badRequest('el tutor está repetido')
+    } else {
+      res.boom.badRequest(err.errors[0].messages[0]);
+    }
   } else {
     
     if(err.code === 11000) {
